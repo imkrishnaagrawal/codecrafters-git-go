d93e4455c22f61dcac15cf86e0ce6e50e107642b
Krishna Agrawal <imkrishnaagrawal@gmail.com>
2025-03-27 11:23:25 +0530

    git stage 5: testing

--- app/main.go
+++ app/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"compress/zlib"
+	"crypto/sha1"
 	"fmt"
 	"io"
 	"log"
@@ -61,7 +62,61 @@ func main() {
 		results := bytes.SplitN(decompressedContent, []byte{0}, 2)
 
 		fmt.Print(string(results[1]))
+	case "hash-object":
+		filepath := os.Args[3]
 
+		file, err := os.Open(filepath)
+		if err != nil {
+			log.Fatal("Unable to read file")
+		}
+		defer file.Close()
+
+		fileContent, err := io.ReadAll(file)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fileSize := len(fileContent)
+
+		// Create the header in the format "blob <size> \0"
+		header := fmt.Sprintf("blob %d\\0", fileSize)
+
+		// Convert the header to a byte slice
+		headerBytes := []byte(header)
+
+		// Combine the header and file content to form the final blob
+		finalBlob := append(headerBytes, fileContent...)
+
+		h := sha1.New()
+		h.Write(fileContent)
+		shaRaw := h.Sum(nil)
+		shaCode := fmt.Sprintf("%x", shaRaw)
+
+		dir := path.Join(".git", "objects", shaCode[:2])
+		outfilePath := path.Join(dir, shaCode[2:])
+
+		if err := os.MkdirAll(dir, 0755); err != nil {
+			log.Fatal("failed to write to objects dir")
+		}
+
+		var b bytes.Buffer
+		w := zlib.NewWriter(&b)
+		w.Write(finalBlob)
+		w.Close()
+
+		outputFile, err := os.Create(outfilePath)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer outputFile.Close()
+
+		// Write the compressed content to the file
+		_, err = outputFile.Write(b.Bytes())
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("%s\n", shaCode)
 	default:
 		fmt.Fprintf(os.Stderr, "Unknown command %s\n", command)
 		os.Exit(1)
